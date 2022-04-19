e2adfb9edc9099dd1f6fdb7809c9e10ba43819d4
Eric Crudup <briefcasemanx@gmail.com>
2022-04-18 17:30:18 -0700

    represents about 6-8 hours of research and messing around in Codepen. Starting Point.

--- __tests__/demo.test.js
+++
@@ -1,9 +0,0 @@
-import Triangle from './../src/js/demo.js';
-
-describe('', () => {
-
-  test('should do this...', () =>{
-    let example = "example text";
-    expect(example).toEqual("example text");
-  });
-});
