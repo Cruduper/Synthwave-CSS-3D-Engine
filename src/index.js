3b3ffe11a2c6b19fe8bfe550c36596ce6181c628
Eric Crudup <briefcasemanx@gmail.com>
2022-04-18 21:29:04 -0700

    creates rudimentary camera control slider

--- src/index.js
+++ src/index.js
@@ -6,5 +6,11 @@ import './css/styles.css';
 
 $(document).ready(function() {
 
+  const slider = document.getElementById("slider");
+  const root = document.documentElement;
+
+  slider.addEventListener("input", (e) => {
+    root.style.setProperty("--sceneRotate", e.target.value + "deg");
+  });
 
 });
