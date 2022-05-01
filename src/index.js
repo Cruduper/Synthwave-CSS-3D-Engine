b8fc573aeb5549d8dd0a5b0d29336ed24124285f
Eric Crudup <briefcasemanx@gmail.com>
2022-05-01 11:37:42 -0700

    adds slider to test left-right player control

--- src/index.js
+++ src/index.js
@@ -6,17 +6,21 @@ import './css/styles.css';
 
 $(document).ready(function() {
 
-  const slider = document.getElementById("angleSlider");
   const root = document.documentElement;
 
+  const slider = document.getElementById("angleSlider");
   slider.addEventListener("input", (e) => {
     root.style.setProperty("--sceneRotate", e.target.value + "deg");
   });
 
   const slider2 = document.getElementById("leftRightSlider");
-
   slider2.addEventListener("input", (e) => {
     root.style.setProperty("--sceneLeftRight", e.target.value + "em");
+  });
+
+  const slider3 = document.getElementById("playerleftRightSlider");
+  slider3.addEventListener("input", (e) => {
+    root.style.setProperty("--playerLeftRight", e.target.value + "em");
   });
 
 });
