336b5b47e4d1ad4fe73140209543e197b7abea3e
Eric Crudup <briefcasemanx@gmail.com>
2022-05-06 12:47:31 -0700

    start of creating an 'eco mode' that disables some effects on press of 'e' button

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 
+
+
+
 $(document).ready(function() {
 
   const root = document.documentElement;
@@ -12,20 +15,38 @@ $(document).ready(function() {
   slider.addEventListener("input", (e) => {
     root.style.setProperty("--sceneRotate", e.target.value + "deg");
   });
-
   const slider2 = document.getElementById("leftRightSlider");
   slider2.addEventListener("input", (e) => {
     root.style.setProperty("--sceneLeftRight", e.target.value + "em");
   });
-
   const slider3 = document.getElementById("playerleftRightSlider");
   slider3.addEventListener("input", (e) => {
     root.style.setProperty("--playerLeftRight", e.target.value + "em");
   });
-
   const slider4 = document.getElementById("distanceSlider");
   slider4.addEventListener("input", (e) => {
     root.style.setProperty("--sceneDistance", e.target.value + "em");
   });
+
+  document.addEventListener('keydown', logKey);
+  console.log(logKey);
+  function logKey(key) {
+  switch(key.code) {
+    case 'KeyE':
+      document.querySelectorAll('.horLine > th').forEach( box => {
+        box.classList.toggle('vertLine');
+      });
+      console.log("eco mode changed");
+      break;
+    default:
+      console.log("!");
+  }
+  return "done";
+}
+
+  
+  // document.getElementById("div1").classList.add("classToBeAdded");
+  // document.getElementById("div1").classList.remove("classToBeRemoved");
+
 
 });
