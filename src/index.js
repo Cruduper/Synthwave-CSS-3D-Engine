37344b1f830bc48505c563b497ad5881066cb892
Eric Crudup <briefcasemanx@gmail.com>
2022-05-19 01:34:10 -0700

    disables keys during intro animation

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ let done = false;
 let keyPress;
 let camAngle = 1;
 let introEnterPressed = false;
+let keysAllowed = false;
 window.onkeyup = function(e) { pressedKeys[e.code] = false; logKey(e);}
 window.onkeydown = function(e) { 
   if (pressedKeys[e.code] === false || pressedKeys[e.code] === undefined){
@@ -45,127 +46,129 @@ function playSong() {
 function logKey(key) {
 
   keyPress = key;
-  //console.log(key.code, introEnterPressed);
-    
-  switch(key.code) {
-    case 'KeyC':
-      if (key.type === 'keydown'){
-        camAngle++;
-        if(camAngle === 1){ 
-        setCameraAngle(-21, -10, 1, 0, 0, 15, 45, 12);
-        document.querySelectorAll('.floor').forEach( function (elem) {
-          elem.style.display = 'block';
-        }); 
-        }
-        else if (camAngle === 2){
-          setCameraAngle(-27, -6, -5, 0, 0, 13, 37, 17);
-        }
-        else if (camAngle === 3){
-          setCameraAngle(-27, -13, -15, -30, 0, 11, 37, 13);
-        }
-        else if (camAngle === 4){
-          setCameraAngle(-20, -3, 30, 0, 0, 17, 37, 17);
-          document.querySelectorAll('.floor').forEach( function (elem) {
-            elem.style.display = 'none';
-          });
-          camAngle = 0;
-        }
-      }
-      break;
-    case 'KeyE':
-      if (key.type === 'keydown')
-      {
-        document.querySelectorAll('.horLine > th').forEach( box => {
-          box.classList.toggle('vertLine');
-        });
-      
-        let exGlowVis = document.querySelector('#floorExtraGlow');
-        let disp = window.getComputedStyle(exGlowVis, null).display;
-        if (disp === "block"){
-          exGlowVis.style.display = "none";
-        } else {
-          exGlowVis.style.display = "block";
-        }
 
-        document.querySelectorAll('#car .face').forEach( face => {
-          face.classList.toggle('playerColor');
-        });
-        document.querySelectorAll("#palmTree [class*='prism']").forEach(face => {
-          face.classList.toggle("leafColor");
-        });
-        document.querySelectorAll("#palmTree [class*='cylinder']").forEach(face => {
-          face.classList.toggle("trunkColor");
-        });
-      }
-      //console.log("eco mode changed");
-      // console.log(key);
-      break;
-    
-    case 'ArrowLeft':
-      if (key.type === "keydown"){
-        car.classList.remove('carBodyTurnLeft', 'carBodyLeftToCenter', 'carBodyTurnRight', 'carBodyRightToCenter');
-        car.classList.add('carBodyTurnLeft')
-        tireFrontLeft.className = "";
-        tireFrontRight.className = "";
-        tireFrontLeft.classList.add('leftTireTurnLeft');
-        tireFrontRight.classList.add('rightTireTurnLeft');
-      }
-      else if (key.type === "keyup"){
-        car.classList.remove('carBodyTurnLeft', 'carBodyLeftToCenter', 'carBodyTurnRight', 'carBodyRightToCenter');
-        car.classList.add('carBodyLeftToCenter')
-        tireFrontLeft.className = "";
-        tireFrontRight.className = "";
-        tireFrontLeft.classList.add('leftTireLeftToCenter');
-        tireFrontRight.classList.add('rightTireLeftToCenter');
-      }
-      window.requestAnimationFrame(step)
-      break;
-    case 'ArrowRight':
-      if (key.type === "keydown"){
-        car.classList.remove('carBodyTurnLeft', 'carBodyLeftToCenter', 'carBodyTurnRight', 'carBodyRightToCenter');
-        car.classList.add('carBodyTurnRight')
-        tireFrontLeft.className = "";
-        tireFrontRight.className = "";
-        tireFrontLeft.classList.add('leftTireTurnRight');
-        tireFrontRight.classList.add('rightTireTurnRight');
-      }
-      else if (key.type === "keyup"){
-        car.classList.remove('carBodyTurnLeft', 'carBodyLeftToCenter', 'carBodyTurnRight', 'carBodyRightToCenter');
-        car.classList.add('carBodyRightToCenter')
-        tireFrontLeft.className = "";
-        tireFrontRight.className = "";
-        tireFrontLeft.classList.add('leftTireRightToCenter');
-        tireFrontRight.classList.add('rightTireRightToCenter');
-      }
-      window.requestAnimationFrame(step)
-      break;
-    case 'ArrowUp':
-      if (key.type === 'keydown'){
-        carContainer.classList.add('boostCar');
-        viewport.classList.add('boostScenePerspective');
-        root.style.setProperty("--floorRate", floorBoostRate);
-        root.style.setProperty("--extraGlowRate", extraGlowBoostRate );
-        root.style.setProperty("--tireRotationRate", tireRotationRateBoost);
+  
+  //console.log(key.code, introEnterPressed);
+  if( keysAllowed === true){  
+    switch(key.code) {
+      case 'KeyC':
+        if (key.type === 'keydown'){
+          camAngle++;
+          if(camAngle === 1){ 
+          setCameraAngle(-21, -10, 1, 0, 0, 15, 45, 12);
+          document.querySelectorAll('.floor').forEach( function (elem) {
+            elem.style.display = 'block';
+          }); 
+          }
+          else if (camAngle === 2){
+            setCameraAngle(-27, -6, -5, 0, 0, 13, 37, 17);
+          }
+          else if (camAngle === 3){
+            setCameraAngle(-27, -13, -15, -30, 0, 11, 37, 13);
+          }
+          else if (camAngle === 4){
+            setCameraAngle(-20, -3, 30, 0, 0, 17, 37, 17);
+            document.querySelectorAll('.floor').forEach( function (elem) {
+              elem.style.display = 'none';
+            });
+            camAngle = 0;
+          }
+        }
+        break;
+      case 'KeyE':
+        if (key.type === 'keydown')
+        {
+          document.querySelectorAll('.horLine > th').forEach( box => {
+            box.classList.toggle('vertLine');
+          });
         
-        document.querySelectorAll('.tire .face').forEach( face => {
-          face.classList.add('boostCarGlow');
-        });
-      }
-      break;
-    case 'Enter':
+          let exGlowVis = document.querySelector('#floorExtraGlow');
+          let disp = window.getComputedStyle(exGlowVis, null).display;
+          if (disp === "block"){
+            exGlowVis.style.display = "none";
+          } else {
+            exGlowVis.style.display = "block";
+          }
+
+          document.querySelectorAll('#car .face').forEach( face => {
+            face.classList.toggle('playerColor');
+          });
+          document.querySelectorAll("#palmTree [class*='prism']").forEach(face => {
+            face.classList.toggle("leafColor");
+          });
+          document.querySelectorAll("#palmTree [class*='cylinder']").forEach(face => {
+            face.classList.toggle("trunkColor");
+          });
+        }
+        //console.log("eco mode changed");
+        // console.log(key);
+        break;
+      
+      case 'ArrowLeft':
+        if (key.type === "keydown"){
+          car.classList.remove('carBodyTurnLeft', 'carBodyLeftToCenter', 'carBodyTurnRight', 'carBodyRightToCenter');
+          car.classList.add('carBodyTurnLeft')
+          tireFrontLeft.className = "";
+          tireFrontRight.className = "";
+          tireFrontLeft.classList.add('leftTireTurnLeft');
+          tireFrontRight.classList.add('rightTireTurnLeft');
+        }
+        else if (key.type === "keyup"){
+          car.classList.remove('carBodyTurnLeft', 'carBodyLeftToCenter', 'carBodyTurnRight', 'carBodyRightToCenter');
+          car.classList.add('carBodyLeftToCenter')
+          tireFrontLeft.className = "";
+          tireFrontRight.className = "";
+          tireFrontLeft.classList.add('leftTireLeftToCenter');
+          tireFrontRight.classList.add('rightTireLeftToCenter');
+        }
+        window.requestAnimationFrame(step)
+        break;
+      case 'ArrowRight':
+        if (key.type === "keydown"){
+          car.classList.remove('carBodyTurnLeft', 'carBodyLeftToCenter', 'carBodyTurnRight', 'carBodyRightToCenter');
+          car.classList.add('carBodyTurnRight')
+          tireFrontLeft.className = "";
+          tireFrontRight.className = "";
+          tireFrontLeft.classList.add('leftTireTurnRight');
+          tireFrontRight.classList.add('rightTireTurnRight');
+        }
+        else if (key.type === "keyup"){
+          car.classList.remove('carBodyTurnLeft', 'carBodyLeftToCenter', 'carBodyTurnRight', 'carBodyRightToCenter');
+          car.classList.add('carBodyRightToCenter')
+          tireFrontLeft.className = "";
+          tireFrontRight.className = "";
+          tireFrontLeft.classList.add('leftTireRightToCenter');
+          tireFrontRight.classList.add('rightTireRightToCenter');
+        }
+        window.requestAnimationFrame(step)
+        break;
+      case 'ArrowUp':
+        if (key.type === 'keydown'){
+          carContainer.classList.add('boostCar');
+          viewport.classList.add('boostScenePerspective');
+          root.style.setProperty("--floorRate", floorBoostRate);
+          root.style.setProperty("--extraGlowRate", extraGlowBoostRate );
+          root.style.setProperty("--tireRotationRate", tireRotationRateBoost);
+          
+          document.querySelectorAll('.tire .face').forEach( face => {
+            face.classList.add('boostCarGlow');
+          });
+        }
+        break;
+      default:
+        // console.log("!");
+    }
+  }
+  else if (key.code === 'Enter'){
+    if (introEnterPressed === false){
       console.log(key.code, introEnterPressed)
-      if (introEnterPressed === false){
-        console.log(key.code, introEnterPressed)
-        introScreen.style.setProperty("display", "none");
-        viewport.style.setProperty("display", "flex");
-        car.classList.add('introAnimationCar');
-        scene.classList.add('introAnimationCamera');
-        introEnterPressed = true;
-        playSong();
-      }
-      break;
-    default:
-      // console.log("!");
+      introScreen.style.setProperty("display", "none");
+      viewport.style.setProperty("display", "flex");
+      car.classList.add('introAnimationCar');
+      scene.classList.add('introAnimationCamera');
+      introEnterPressed = true;
+      playSong();
+    }
   }
 }
 
@@ -258,6 +261,16 @@ $(document).ready(function() {
       root.style.setProperty("--floorRate", floorRate);
       root.style.setProperty("--extraGlowRate", extraGlowRate);
       root.style.setProperty("--tireRotationRate", tireRotationRate);
+    }
+
+
+    //car.classList.add('introAnimationCar');
+  });
+
+  car.addEventListener("animationend", (event)=>{
+    if(event.animationName === 'introAnimationCar'){
+      car.classList.remove('introAnimationCar');
+      keysAllowed = true;
     }
   });
 })
